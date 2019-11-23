3d65f639ee03438be432e22fbcbf483aa6c6a952
Asset <w.aset.kz@gmail.com>
2019-11-23 19:46:54 +0600

    search wip

--- app/src/main/java/com/example/letsplay/ui/search/SearchActivity.kt
+++ app/src/main/java/com/example/letsplay/ui/search/SearchActivity.kt
@@ -1,13 +1,27 @@
 package com.example.letsplay.ui.search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import com.example.letsplay.R
+import com.example.letsplay.helper.utility.gone
+import com.example.letsplay.ui.common.BaseActivity
+import kotlinx.android.synthetic.main.activity_search.*
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        back.setOnClickListener {
+            finish()
+        }
+
+        clear.gone()
+
+        clear.setOnClickListener {
+            search.setText("")
+        }
+
     }
 }
